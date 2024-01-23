b340697b00762b1ce9bc14e06f3f0316181dac17
blackdayalpha <93610550+blackdayalpha@users.noreply.github.com>
2024-01-23 09:40:44 +0530

    Update loan_app.js

--- loan_app.js
+++ loan_app.js
@@ -4,21 +4,37 @@ let interest_rate;
 let calculated_emi = 0;
 
 function calculate_interest() {
-  if (check()) {
-    loan_amount = document.getElementById("loan_amount");
-    interest_rate = document.getElementById("interest_rate");
-    tenure = document.getElementById("loan_year");
-    console.log(loan_amount.value);
-    console.log(interest_rate.value);
-    console.log(tenure.value);
-    //   calculate(loan_amount, interest_rate, tenure);
-    document.getElementById("emi_amount").innerText = 10;
-  } 
+  loan_amount = document.getElementById("amount").value;
+  interest_rate = document.getElementById("interest").value;
+  tenure = document.getElementById("tenure").value;
+  //   console.log(loan_amount.value);
+  //   console.log(interest_rate.value);
+  //   console.log(tenure.value);
+  let emi = calculate(
+    Number(loan_amount),
+    Number(interest_rate),
+    Number(tenure)
+  );
+  console.log(emi);
+  document.getElementById("emi_amount").innerText = emi.toFixed(2);
 }
 
-function check() {
-  if (loan_amount === null || interest_rate === null || tenure === null) {
-    alert("Please fill out all the value");
-    return false;
-  } else return true;
+function calculate(p, r, n) {
+  console.log(p);
+  console.log(r);
+  console.log(n);
+  //   let sum = 1 + r;
+  //   console.log(sum);
+  //   console.log(Math.pow(sum, n));
+  //   console.log(p * r * Math.pow(1 + r, n));
+  //   console.log(Math.pow(1 + r, n) - 1);
+
+  return Number((p * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1));
 }
+
+// function check() {
+//   if (loan_amount === null || interest_rate === null || tenure === null) {
+//     alert("Please fill out all the value");
+//     return false;
+//   } else return true;
+// }
